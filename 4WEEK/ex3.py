32cd0c570be24710ed6dc3f1712011ad3a3dfbba
Y0-0N653 <boyun1625@naver.com>
2024-09-26 12:22:50 +0900

    4주차 실습 파일

---
+++ 4WEEK/ex3.py
@@ -0,0 +1,15 @@
+def sum_int(a, b):
+  return a + b
+
+num1 = 10
+num2 = 20
+
+result = sum_int(num1, num2)
+print(f'{num1} + {num2} = {result}')
+
+# 곱하기
+def mul(a, b):
+  return a * b
+
+result = mul(num1, num2)
+print(f'{num1} x {num2} = {result}')
---
+++ 4WEEK/ex4.py
@@ -0,0 +1,17 @@
+# 임의의 정수를 받아서 1~정수까지 합을 구하는 프로그램
+# 정수의 합을 구하는 로직은 함수(sumfunc)로 만들기
+
+num = int(input("정수를 입력해주세요."))
+
+def sumfunc(num) :
+  if num > 0 :
+    total = 0
+    for i in range (1, num + 1) :
+      total += i
+    return total
+  else :
+    print("정수를 다시 입력해주세요.")
+    
+result = sumfunc(num)
+print("1부터 ", num, "까지의 합 >> ", result)
+

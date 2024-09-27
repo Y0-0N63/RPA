c9034dbeb275cf105c694b3e06704b3bc795bf4e
Y0-0N653 <boyun1625@naver.com>
2024-09-27 10:37:22 +0900

    4주차 실습 파일

--- 4WEEK/ex4.py
+++ 4WEEK/ex4.py
@@ -1,17 +1,16 @@
 # 임의의 정수를 받아서 1~정수까지 합을 구하는 프로그램
 # 정수의 합을 구하는 로직은 함수(sumfunc)로 만들기
 
-num = int(input("정수를 입력해주세요."))
+num = int(input("정수를 입력해주세요. >> "))
 
 def sumfunc(num) :
-  if num > 0 :
-    total = 0
-    for i in range (1, num + 1) :
-      total += i
-    return total
-  else :
-    print("정수를 다시 입력해주세요.")
-    
-result = sumfunc(num)
-print("1부터 ", num, "까지의 합 >> ", result)
+  total = 0
+  for i in range(1, num + 1) :
+    total += i
+  return total
 
+if num < 0 :
+  print ("정수를 다시 입력해주세요.")
+else : 
+  result = sumfunc(num)
+  print("1부터 ", num, "까지의 합 >> ", result)
